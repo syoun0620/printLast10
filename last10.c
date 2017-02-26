3fbd8a07fe555f8b532b0247496b4a5373cd942b
sukwhan (david) <younsa@bc.edu>
2017-02-26 00:40:44 -0500

    Add files via upload

---
+++ last10.c
@@ -0,0 +1,49 @@
+
+// Sukwhan Youn
+// Computer Systems HW3 
+// #2
+
+#include <stdio.h>
+
+int array1[10];
+int *p;			//pointer to array
+int i;
+int number;     //input
+int counter;
+
+int main(){
+	p=array1;
+	scanf("%d", &number);
+    int i=0;
+    counter++;
+	while (number != 0){
+		*(p+i)=number;
+        i++;
+        counter++;
+		scanf("%d", &number);
+        if (i>9){
+            i=0;
+        }
+	}
+
+	//printing    
+    printf("\n");
+    if (counter <10){
+        for (int y=0;y<counter;y++){
+            if (*(p+y) !=0){
+                printf("%d ",*(p+y));
+            }
+        }
+    }
+	for (int z=0;z<10;z++){
+        if (*(p+i) != 0){
+		    printf("%d ",*(p+i));
+            i++;
+        }
+        if (i>9){
+            i=0;
+        }
+	}
+    printf("\n");
+
+}
